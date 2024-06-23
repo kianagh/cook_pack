7b5e7c2e151c9f1f06b1c7a104e47906d3ae0238
kiana <k.gharaeian@gmail.com>
2024-06-23 18:23:49 +0330

    check

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/foods" element={<Foods />} />
         <Route path="/register" element={<Registering/>}/>
-
       </Routes>
     </div>
   );
