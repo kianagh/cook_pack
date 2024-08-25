e75668a79e73339482d9e65d171b2a536562b8be
kiana <k.gharaeian@gmail.com>
2024-08-25 19:24:56 +0330

    start back-end

--- back-end/cookpack_project/food/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Food
-# Register your models here.
-
-class FoodAdmin(admin.ModelAdmin):
-    list_display = ("name","time","is_active")
-
-admin.site.register(Food, FoodAdmin)
--- back-end/cookpack_project/food/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .serializers import FoodSerializer
-from .models import Food
-
-class FoodView(viewsets.ModelViewSet):
-    serializer_class = FoodSerializer
-    queryset = Food.objects.filter(is_active=True)
